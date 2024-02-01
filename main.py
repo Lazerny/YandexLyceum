6168e5f878e3e2fa1a6060e34053745adcd79526
Jopkins <justfredbear@gmail.com>
2024-02-01 17:32:01 +0300

    task 3

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from main import show_map
+from PyQt5 import QtCore
+
+from second import show_map
 
 import os
 import sys
@@ -12,30 +14,52 @@ SCREEN_SIZE = [600, 450]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.ll = "37.530887,55.703118"
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        self.ll = [37.530887, 55.703118]
         self.getImage()
-        self.initUI()
+        self.showMap()
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key.Key_Left:
+            self.ll[0] -= self.ll[0] * 0.00001
+            self.getImage()
+            self.showMap()
+            pass
+        elif event.key() == QtCore.Qt.Key.Key_Right:
+            self.ll[0] += self.ll[0] * 0.00001
+            self.getImage()
+            self.showMap()
+            pass
+        elif event.key() == QtCore.Qt.Key.Key_Up:
+            self.ll[1] += self.ll[1] * 0.00001
+            self.getImage()
+            self.showMap()
+            pass
+        elif event.key() == QtCore.Qt.Key.Key_Down:
+            self.ll[1] -= self.ll[1] * 0.00001
+            self.getImage()
+            self.showMap()
+            pass
 
     def getImage(self):
-        response = show_map(ll=self.ll)
+        response = show_map(ll=','.join(list(map(str, self.ll))))
 
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response)
 
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        # Изображение
+    def showMap(self):
         self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
+        self.image.show()
+        self.image.update()
 
     def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
 
